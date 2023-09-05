45c82c2a454f73e565bb699e6354078791c4e394
Thuy Quematon <thuytkq@outlook.com>
2023-09-05 23:43:27 +0930

    u

---
+++ oceano/src/main.c
@@ -0,0 +1,25 @@
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+
+	a = NULL;
+	b = NULL;
+	if (1 == argc || (2 == argc && !argv[1][0]))
+		return (1);
+	else if (2 == argc)
+		argv = ft_split(argv[1], ' ');
+	stack_init(&a, argv + 1, 2 == argc);
+	if (!stack_sorted(a))
+	{
+		if (stack_len(a) == 2)
+			sa(&a, false);
+		else if (stack_len(a) == 3)
+			tiny_sort(&a);
+		else
+			push_swap(&a, &b);
+	}
+	free_stack(&a);
+}
