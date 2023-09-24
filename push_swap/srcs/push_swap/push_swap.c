335231cbc99734e4dea6fb019cdb942aba0e6f81
Thuy Quematon <thuytkq@outlook.com>
2023-09-24 17:08:39 +0930

    u

--- push_swap/srcs/push_swap/push_swap.c
+++ push_swap/srcs/push_swap/push_swap.c
@@ -8,10 +8,7 @@ int	main(int argc, char **argv)
 	a = NULL;
 	b = NULL;
 	if (argc == 1 || (argc == 2 && !argv[1][0]))
-	{
-		ft_printf("Error: Invalid input\nTry: ./push_swap <random numbers> \n");
 		return (1);
-	}
 	else if (argc == 2)
 		argv = split(argv[1], ' ');
 	init_stack_a(&a, argv + 1);
